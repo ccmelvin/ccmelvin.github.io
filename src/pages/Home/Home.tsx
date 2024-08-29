a7e98cad470554c2a4b62c90414d7d488dcd0c48
cassia <cassiamelvin@gmail.com>
2024-08-29 15:59:15 -0400

    created hook usePageLeave.ts to show modal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+// import ResponsiveComponent from '../../components/ResponsiveComponent/ResponsiveComponent'
+
 import styles from './Home.module.css'
+import ExitWarningModal from '../../components/ExitWarningModal/ExitWarningModal'
 
 const Home: React.FC = () => {
   return (
     <div className={styles.home}>
       <p>Home</p>
       <Header />
+      {/* <ResponsiveComponent /> */}
+      <ExitWarningModal />
     </div>
   )
 }
